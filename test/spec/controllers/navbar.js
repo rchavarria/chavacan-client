45d4e0c021753dd10d41e60f521ea733ec369421
rchavarria <txingo@gmail.com>
2014-02-16 18:24:25 +0100

    add a couple of test for 'isActive' method of NavbarCtrl

--- test/spec/controllers/navbar.js
+++ test/spec/controllers/navbar.js
@@ -2,21 +2,29 @@
 
 describe('Controller: NavbarCtrl', function () {
 
-  // load the controller's module
   beforeEach(module('chavacanClientApp'));
 
   var NavbarCtrl,
-    scope;
+    scope,
+    mockLocation;
 
   // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
+  beforeEach(inject(function ($controller, $rootScope, $location) {
+    mockLocation = $location;
+
     scope = $rootScope.$new();
     NavbarCtrl = $controller('NavbarCtrl', {
       $scope: scope
     });
   }));
 
-  it('should attach a list of awesomeThings to the scope', function () {
-    expect(scope.awesomeThings.length).toBe(3);
+  it('should return true when location matches', function () {
+    mockLocation.path('/');
+    expect(scope.isActive('/')).toBeTruthy();
+  });
+
+  it('should return false when location does not match', function () {
+    mockLocation.path('/');
+    expect(scope.isActive('/foo')).toBeTruthy();
   });
 });
