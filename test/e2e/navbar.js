df3ce49e07c1bb84407703c47e8100770b515e39
rchavarria <txingo@gmail.com>
2014-02-27 23:56:05 +0100

    refactor: extract a lot of duplicated code to a method

--- test/e2e/navbar.js
+++ test/e2e/navbar.js
@@ -1,93 +1,62 @@
 describe('Navigation Bar', function() { 
 
+  var checkActive = function (tabIndex) {
+    for(var i = 1; i < totalTabs; i++) {
+      var elementSelector = '.nav li:nth-child(' + i + ')';
+      if (i === tabIndex) {
+        expect(element(elementSelector).attr('class')).toContain('active');
+      } else {
+        expect(element(elementSelector).attr('class')).not().toBeDefined();
+      }
+    }
+  };
+
   it('selects "Home" if location is /', function() { 
     browser().navigateTo('/'); 
     expect(browser().location().path()).toBe('/'); 
 
-    expect(element('.nav li:nth-child(1)').attr('class')).toContain('active'); 
-    expect(element('.nav li:nth-child(2)').attr('class')).not().toBeDefined();
-    expect(element('.nav li:nth-child(3)').attr('class')).not().toBeDefined(); 
-    expect(element('.nav li:nth-child(4)').attr('class')).not().toBeDefined(); 
-    expect(element('.nav li:nth-child(5)').attr('class')).not().toBeDefined(); 
-    expect(element('.nav li:nth-child(6)').attr('class')).not().toBeDefined(); 
-    expect(element('.nav li:nth-child(7)').attr('class')).not().toBeDefined();
+    checkActive(1);
   });
 
   it('selects "Alquileres" if location is /rentings', function() { 
     browser().navigateTo('/#/rentings'); 
     expect(browser().location().path()).toBe('/rentings');
 
-    expect(element('.nav li:nth-child(1)').attr('class')).not().toBeDefined();
-    expect(element('.nav li:nth-child(2)').attr('class')).toContain('active'); 
-    expect(element('.nav li:nth-child(3)').attr('class')).not().toBeDefined(); 
-    expect(element('.nav li:nth-child(4)').attr('class')).not().toBeDefined(); 
-    expect(element('.nav li:nth-child(5)').attr('class')).not().toBeDefined(); 
-    expect(element('.nav li:nth-child(6)').attr('class')).not().toBeDefined(); 
-    expect(element('.nav li:nth-child(7)').attr('class')).not().toBeDefined();
+    checkActive(2);
   });
 
   it('selects "Ventas" if location is /sales', function() { 
     browser().navigateTo('/#/sales'); 
     expect(browser().location().path()).toBe('/sales');
 
-    expect(element('.nav li:nth-child(1)').attr('class')).not().toBeDefined();
-    expect(element('.nav li:nth-child(2)').attr('class')).not().toBeDefined(); 
-    expect(element('.nav li:nth-child(3)').attr('class')).toContain('active'); 
-    expect(element('.nav li:nth-child(4)').attr('class')).not().toBeDefined(); 
-    expect(element('.nav li:nth-child(5)').attr('class')).not().toBeDefined(); 
-    expect(element('.nav li:nth-child(6)').attr('class')).not().toBeDefined(); 
-    expect(element('.nav li:nth-child(7)').attr('class')).not().toBeDefined();
+    checkActive(3);
   });
 
   it('selects "Servicios Inmoviliarios" if location is /services', function() { 
     browser().navigateTo('/#/services'); 
     expect(browser().location().path()).toBe('/services');
 
-    expect(element('.nav li:nth-child(1)').attr('class')).not().toBeDefined();
-    expect(element('.nav li:nth-child(2)').attr('class')).not().toBeDefined(); 
-    expect(element('.nav li:nth-child(3)').attr('class')).not().toBeDefined(); 
-    expect(element('.nav li:nth-child(4)').attr('class')).toContain('active'); 
-    expect(element('.nav li:nth-child(5)').attr('class')).not().toBeDefined(); 
-    expect(element('.nav li:nth-child(6)').attr('class')).not().toBeDefined(); 
-    expect(element('.nav li:nth-child(7)').attr('class')).not().toBeDefined();
+    checkActive(4);
   });
 
   it('selects "Quiere vender" if location is /wanttosell', function() { 
     browser().navigateTo('/#/wanttosell'); 
     expect(browser().location().path()).toBe('/wanttosell');
 
-    expect(element('.nav li:nth-child(1)').attr('class')).not().toBeDefined();
-    expect(element('.nav li:nth-child(2)').attr('class')).not().toBeDefined(); 
-    expect(element('.nav li:nth-child(3)').attr('class')).not().toBeDefined(); 
-    expect(element('.nav li:nth-child(4)').attr('class')).not().toBeDefined(); 
-    expect(element('.nav li:nth-child(5)').attr('class')).toContain('active'); 
-    expect(element('.nav li:nth-child(6)').attr('class')).not().toBeDefined(); 
-    expect(element('.nav li:nth-child(7)').attr('class')).not().toBeDefined();
+    checkActive(5);
   });
 
   it('selects "About" if location is /about', function() { 
     browser().navigateTo('/#/about'); 
     expect(browser().location().path()).toBe('/about');
 
-    expect(element('.nav li:nth-child(1)').attr('class')).not().toBeDefined();
-    expect(element('.nav li:nth-child(2)').attr('class')).not().toBeDefined(); 
-    expect(element('.nav li:nth-child(3)').attr('class')).not().toBeDefined(); 
-    expect(element('.nav li:nth-child(4)').attr('class')).not().toBeDefined(); 
-    expect(element('.nav li:nth-child(5)').attr('class')).not().toBeDefined(); 
-    expect(element('.nav li:nth-child(6)').attr('class')).toContain('active'); 
-    expect(element('.nav li:nth-child(7)').attr('class')).not().toBeDefined();
+    checkActive(6);
   });
 
   it('selects "Contact" if location is /contact', function() { 
     browser().navigateTo('/#/contact'); 
+    expect(browser().location().path()).toBe('/contact');
 
-    expect(browser().location().path()).toBe('/contact'); 
-    expect(element('.nav li:nth-child(1)').attr('class')).not().toBeDefined();
-    expect(element('.nav li:nth-child(2)').attr('class')).not().toBeDefined(); 
-    expect(element('.nav li:nth-child(3)').attr('class')).not().toBeDefined(); 
-    expect(element('.nav li:nth-child(4)').attr('class')).not().toBeDefined(); 
-    expect(element('.nav li:nth-child(5)').attr('class')).not().toBeDefined(); 
-    expect(element('.nav li:nth-child(6)').attr('class')).not().toBeDefined(); 
-    expect(element('.nav li:nth-child(7)').attr('class')).toContain('active'); 
+    checkActive(7);
   });
 });
