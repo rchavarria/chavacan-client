b21600d16a4472543c3a1490eadf76d74f3fd448
rchavarria <txingo@gmail.com>
2014-03-13 00:05:19 +0100

    add a nodejs express server

---
+++ server.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var app = express();
+
+// New call to compress content
+app.use(express.compress());
+
+app.use(express.static(__dirname + '/dist'));
+
+app.listen(process.env.PORT || 9000);
